051b2408fc533e429dd8ce702d9f9475827767cd
shirAkselrad <shir556202@gmail.com>
2026-08-17 16:39:35 +0300

    17-08 בדיקת קומפוננטות

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,17 @@
 import "../App/app.module.css";
-import Header from "../components/Header/Header.jsx"
-import ShopPage from "../components/Shop-Page/ShopPage/ShopPage.jsx";
-import InfoPopup from "../components/Shop-Page/Info-Popup/InfoPopup/InfoPopup.jsx";
-import Cart from "../components/General/CartWindow/Cart/Cart.jsx";
+
+import Header from "../components/Header/Header.jsx";
+import EmployeePage from "../components/Employee-Page/EmployeePage/EmployeePage.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
-import products from "../components/Shop-Page/proudcts.js";
+
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <ShopPage/>
-      <Cart/>
-      <InfoPopup product={products[1]} />
+      <Header />
+
+      <EmployeePage />
+
       <Footer />
     </div>
   );
