c1b6e9325fafa72e6e466696d6914d23c56e1694
shirAkselrad <shir556202@gmail.com>
2026-08-18 19:56:46 +0300

    18-08 התחלת בניית ריאקט ראוטר

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,50 @@
 import "../App/app.module.css";
-
 import Header from "../components/Header/Header.jsx";
 import EmployeePage from "../components/Employee-Page/EmployeePage/EmployeePage.jsx";
 import Footer from "../components/Footer/Footer.jsx";
-
+import ShopPage from "../components/Shop-Page/ShopPage/ShopPage.jsx";
+import AboutUsPage from "../components/AboutUs-Page/AboutUsPage/AboutUsPage.jsx";
+import ManagerPage from "../components/Manager-Page/ManagerPage/ManagerPage.jsx";
+import LoginPage from "../components/Login-Page/LoginPage/LoginPage.jsx";
+import RegisterPage from "../components/Register-Page/RegisterPage/RegisterPage.jsx";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
+  const hideHeader =
+    location.pathname === "/loginPage" || location.pathname === "/registerPage";
   return (
-    <div className="app">
-      <Header />
-
-      <EmployeePage />
-
+    <div>
+      {!hideHeader && <Header />}
+      <Routes>
+        {/*Shop page */}
+        <Route path="/" element={<ShopPage />} />
+        {/*About us*/}
+        <Route path="/aboutUs" element={<AboutUsPage />} />
+        {/*Manager Page*/}
+        <Route path="managerPage" element={<ManagerPage />} />
+        {/*Employee Page*/}
+        <Route path="employeePage" element={<EmployeePage />} />
+        {/*Login Page*/}
+        <Route path="/loginPage" element={<LoginPage />} />
+        {/*Register Page*/}
+        <Route path="/registerPage" element={<RegisterPage />} />
+      </Routes>
       <Footer />
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="app">
+//       <Header />
+
+//       <EmployeePage />
+
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
