ec4bf8c0c398e0a98f9eaffd805bb2ffb0e51115
shirAkselrad <shir556202@gmail.com>
2026-08-27 12:09:32 +0300

    27-08

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,52 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
+
+    proxy: {
+      "/register": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/login": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/shop": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/employee": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/manager": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/forgotPassword": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/recoveryPage": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+
+      "/resetPassword": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
   },
-});  
+});
