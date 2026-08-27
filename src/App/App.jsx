2be51d0c1dd6f8dc9e0e41f4dccff3c25b8fce85
shirAkselrad <shir556202@gmail.com>
2026-08-27 12:10:22 +0300

    27-08 עדכון הראוטר של ריאקט
    הוספת נתיבים נוספים חדשים

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,13 +7,20 @@ import AboutUsPage from "../components/AboutUs-Page/AboutUsPage/AboutUsPage.jsx"
 import ManagerPage from "../components/Manager-Page/ManagerPage/ManagerPage.jsx";
 import LoginPage from "../components/Login-Page/LoginPage/LoginPage.jsx";
 import RegisterPage from "../components/Register-Page/RegisterPage/RegisterPage.jsx";
+import ForgotPasswordPage from "../components/ForgotPassword-Page/ForgotPasswordPage/ForgotPasswordPage.jsx";
+import RecoveryPage from "../components/Recovery-Page/RecoveryPage/RecoveryPage.jsx";
+import ResetPasswordPage from "../components/ResetPassword-Page/ResetPasswordPage/ResetPasswordPage.jsx";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
   const location = useLocation();
 
   const hideHeader =
-    location.pathname === "/loginPage" || location.pathname === "/registerPage";
+    location.pathname === "/loginPage" ||
+    location.pathname === "/registerPage" ||
+    location.pathname === "/ForgotPasswordPage" ||
+    location.pathname === "/recoveryPage" ||
+    location.pathname === "/ResetPasswordPage";
   return (
     <div>
       {!hideHeader && <Header />}
@@ -30,21 +37,15 @@ function App() {
         <Route path="/loginPage" element={<LoginPage />} />
         {/*Register Page*/}
         <Route path="/registerPage" element={<RegisterPage />} />
+        {/*Forgot Password Page*/}
+        <Route path="/ForgotPasswordPage" element={<ForgotPasswordPage />} />
+        {/*Recovery Page */}
+        <Route path="/recoveryPage" element={<RecoveryPage />} />
+        {/**Reset Password Page */}
+        <Route path="/ResetPasswordPage" element={<ResetPasswordPage />} />
       </Routes>
       <Footer />
     </div>
   );
 }
-// function App() {
-//   return (
-//     <div className="app">
-//       <Header />
-
-//       <EmployeePage />
-
-//       <Footer />
-//     </div>
-//   );
-// }
-
 export default App;
